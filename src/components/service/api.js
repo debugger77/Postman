a09cba5c48e1e2b7f376cca15d9696d6eb409ce3
debugger77 <devhr78@gmail.com>
2023-04-07 16:07:46 +0530

    postman-clone created

---
+++ src/components/service/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { getHeadersAndParams } from '../utils/common-utils';
+
+export const getData = async (formData, jsonText, paramData, headerData) => {
+
+    const apiType = formData.type.toLowerCase();
+    const apiURL = formData.url;
+    const apiHeaders = getHeadersAndParams(headerData);
+    const apiParams = getHeadersAndParams(paramData);
+
+    try {
+        return await axios({
+            method: apiType,
+            url: apiURL,
+            body: jsonText,
+            headers: apiHeaders,
+            params: apiParams
+        })
+
+    } catch(error) {
+        console.log('Error while calling getData API', error);
+        return 'error';
+    }
+}
